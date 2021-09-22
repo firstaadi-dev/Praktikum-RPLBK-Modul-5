67246f5e4bec0f81f7e8e200b789e84f914baff4
Firsta Adi Pradana <firstaadi@hotmail.com>
2021-09-22 08:27:40 +0700

    upload tugas 5

--- tugas-modul5-kel40/src/App.js
+++ tugas-modul5-kel40/src/App.js
@@ -1,17 +1,17 @@
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import './App.css'
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import "./App.css";
 
 function App() {
   return (
     <div>
       <AppBar color="primary" position="static">
-      <Toolbar variant="dense">
-      <Typography variant="h4" color="secondary" component="div">
-        Tugas Kelompok 40 Modul 5
-      </Typography>
-      </Toolbar>
+        <Toolbar variant="dense">
+          <Typography variant="h4" color="secondary" component="div">
+            Tugas Kelompok 40 Modul 5
+          </Typography>
+        </Toolbar>
       </AppBar>
       <br></br>
     </div>
